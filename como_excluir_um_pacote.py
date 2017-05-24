caeb299b8f8defe6158fe8a527df7a21a74ac2d1
Cícero Raupp Rolim <ironworld@gmail.com>
2017-05-24 15:30:26 -0300

    Exemplo de como excluir um pacote.

---
+++ como_excluir_um_pacote.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+
+import requests
+
+
+if __name__ == "__main__":
+    host = "www.muambator.com.br"
+    api_token = "insira-seu-token-aqui"
+    codigo_pacote = "PL970650197BR"
+    r = requests.delete('http://%s/api/clientes/v1/pacotes/%s/?api-token=%s' % (host, codigo_pacote, api_token), headers={"Content-type": "application/json"})
+    print r.status_code
+    print r.text
